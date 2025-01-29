958033dc8c9aa73b6987a3f770affd4eba2041c2
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-01-29 08:23:39 +0530

    This 4th Commit

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 import express from "express"
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: "30mb",
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "30mb"
+}));
+
+// This For Public Assets Like Image, Favicon, Some Files Agar Mere hi Server Par Store Rakhna Chahta Hu Isliye.
+
+app.use(express.static("public"));
+
+// Basically User Ke Cookies Par CRUD Operation Perform Karne ke liye.
+app.use(cookieParser())
+
+
+
+
+
+
 
 export {app};
 
--- db/index.db.js
+++ db/index.db.js
@@ -11,6 +11,7 @@ const connectDB = async()=>{
     }
 };
 
+
 export default connectDB; 
 
 
